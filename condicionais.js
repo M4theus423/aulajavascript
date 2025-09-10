91805a48e3d707e46cf5824840fdf4809b24a11d
M4theus423 <matheus_f_mueller@estudante.sesisenai.org.br>
2025-09-10 23:54:13 +0000

    Adicionar variáveis iniciais e logs de tipo em JavaScript

---
+++ condicionais.js
@@ -0,0 +1,19 @@
+/*
+
+    se (nota >= 7.) {
+        escreva("Aprovado!")
+    } senao {
+        escreva("Reprovado!")
+    }
+
+*/
+
+let nota = 7.0;
+
+if (nota >= 7.0) {  
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
+
+console.log(typeof nota);
---
+++ exercicio-prático-um.js
@@ -0,0 +1,10 @@
+let meuNome = "Matheus Mueller";
+let minhaIdade = 18;
+let minhaCidade = "Pomerode";
+let gostaDeProgramar = true;
+
+!gostaDeProgramar
+
+console.log('Perfil: ${meuNome}, $*{minhaIdade} anos!');
+console.log('Cidade: ${minhaCidade}');
+console.log('Gosta de programar: ${minhaCidade}');
