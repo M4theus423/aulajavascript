50cc0920f5e153464bf3e71d163f1f43481ac78e
M4theus423 <matheus_f_mueller@estudante.sesisenai.org.br>
2025-09-10 19:36:16 -0300

    Add README and initial variable declarations in JavaScript

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+// Portugol para JavaScript
+// Declaração de variáveis
+
+/* Em portugol:
+cadeia nome = "João"
+cadeia sobrenome = "Silva"
+inteiro idade = 25
+real altura = 1.75
+lógico ehHumano = verdadeiro
+*/
+
+
+let nome
+let sobrenome
+let idade
+let altura
+let ehHumano
+
+nome = "João"
+sobrenome = "Silva"
+idade = 25
+altura = 1.75
+ehHumano = true
+
+
+console.log("Nome:", nome); //escreva("Nome:", nome)
+console.log("Sobrenome:", sobrenome); //escreva("Sobrenome:", sobrenome)
+console.log("Idade:", idade); //escreva("Idade:", idade)
+console.log("Altura:", altura); //escreva("Altura:", altura)
+console.log("É estudante:", ehHumano); //escreva("É Humano:", ehHumano)
